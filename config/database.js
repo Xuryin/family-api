3a9e7d8418fcd7558b09bd88802fe5a730dfcebb
xuryin <2658125002@qq.com>
2018-10-08 16:33:18 +0800

    一些修改

---
+++ config/database.js
@@ -0,0 +1,9 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        host: '47.95.234.135',
+        user: 'xuryin',
+        password: 'xuryin0127',
+        database: 'xy'
+    }
+};
